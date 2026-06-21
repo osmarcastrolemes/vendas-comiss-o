ef2ab10a200ef2b7b0beec320bf460643884bd5f
osmarcastrolemes <osmar.proinfo@gmail.com>
2026-06-20 21:43:22 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+require('./src/config/db');
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
